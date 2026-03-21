cab0c576defe6ee4d4e5e60a6f8f83790b487128
hubertbanas <2602582+hubertbanas@users.noreply.github.com>
2026-03-20 20:23:30 -0400

    perf: preload robot sprites to prevent visual flickering (#39)
    - Implement image preloading for directional robot sprites (up, down, left, right)
    - Cache assets in the browser on initial game load
    - Eliminate the momentary blank flicker during the player's first moves

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,27 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Game from "./Game";
 import { ThemeProvider } from "./hooks/theme";
+import { preloadGameAssets } from "./utils/preload-assets";
 
-const rootElement = document.getElementById("root");
+async function bootstrap() {
+  const rootElement = document.getElementById("root");
+  if (!rootElement) {
+    throw new Error("Root element not found");
+  }
 
-if (!rootElement) {
-  throw new Error("Root element not found");
+  const loadingElement = document.getElementById("boot-loading");
+
+  await preloadGameAssets();
+
+  createRoot(rootElement).render(
+    <React.StrictMode>
+      <ThemeProvider>
+        <Game />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+
+  loadingElement?.remove();
 }
 
-createRoot(rootElement).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <Game />
-    </ThemeProvider>
-  </React.StrictMode>
-);
+void bootstrap();
